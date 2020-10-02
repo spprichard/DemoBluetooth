a462c388bb6a30cc2af87dce2786f58503c205fc
Steven Prichard <spprichard20@gmail.com>
2020-10-02 18:27:10 -0500

    Initial Commit

---
+++ DemoBluetooth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoBluetooth
+//
+//  Created by Steven Prichard on 10/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoBluetooth/DemoBluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoBluetoothApp.swift
+//  DemoBluetooth
+//
+//  Created by Steven Prichard on 10/2/20.
+//
+
+import SwiftUI
+
+@main
+struct DemoBluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
